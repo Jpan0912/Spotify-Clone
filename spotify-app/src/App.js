1119184b89220cb45c87b57541e8cb449552aeb6
Jpan0912 <74218391+Jpan0912@users.noreply.github.com>
2022-09-01 22:46:25 +1200

    getting token

--- spotify-app/src/App.js
+++ spotify-app/src/App.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Login from "./components/Login";
+import { reducerCases } from "./utils/Constant";
+import { useStateProvider } from "./utils/StateProvider";
+
 export default function App() {
+  const [{ token }, dispatch] = useStateProvider()
+  useEffect(()=> {
+    const hash = window.location.hash;
+    if(hash){
+      const token = hash.substring(1).split("&")[0].split('=')[1]
+      console.log(token);
+      dispatch({action:reducerCases.SET_TOKEN, token});
+    }
+  }, [token , dispatch]);
   return (
     <div>
       <Login />
