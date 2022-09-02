0862fe659d5b72337b2a605006a644cc0c652fc4
Jpan0912 <74218391+Jpan0912@users.noreply.github.com>
2022-09-03 03:24:00 +1200

    styling componenets

--- spotify-app/src/App.js
+++ spotify-app/src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from "react";
 import Login from "./components/Login";
+import Spotify from "./components/Spotify";
 import { reducerCases } from "./utils/Constant";
 import { useStateProvider } from "./utils/StateProvider";
 
@@ -10,12 +11,13 @@ export default function App() {
     if(hash){
       const token = hash.substring(1).split("&")[0].split('=')[1]
       console.log(token);
-      dispatch({action:reducerCases.SET_TOKEN, token});
+      dispatch({type:reducerCases.SET_TOKEN, token});
     }
   }, [token , dispatch]);
   return (
     <div>
-      <Login />
+      {token ? <Spotify /> : <Login /> }
+      
     </div>
   );
 }
