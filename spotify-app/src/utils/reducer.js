3baafdd378acc14c12cf6c97d262610dc2f9e314
Jpan0912 <74218391+Jpan0912@users.noreply.github.com>
2022-09-05 13:05:00 +1200

    implenting styling on sidebar

--- spotify-app/src/utils/reducer.js
+++ spotify-app/src/utils/reducer.js
@@ -2,13 +2,20 @@ import { reducerCases } from "./Constant";
 
 export const initialState = {
   token: null,
-};
+  playlists: []
+,};
 
 const reducer = (state, action) => {
   switch (action.type) {
     case reducerCases.SET_TOKEN : {
       return {
         ...state,token:action.token,
+      }
+    }
+    case reducerCases.SET_PLAYLISTS : {
+      return {
+        ...state,
+        playlists:action.playlists,
       }
     }
     default:
