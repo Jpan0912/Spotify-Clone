31d6fe5f1649eabe99be11209248d088a7a87a23
John Pan <pan@Johns-MacBook-Pro-4.local>
2022-10-07 12:55:53 +1300

    body implementation

--- spotify-app/src/components/Spotify.jsx
+++ spotify-app/src/components/Spotify.jsx
@@ -1,76 +1,80 @@
-import React, {useEffect} from 'react'
-import styled from 'styled-components';
-import Navbar from './Navbar';
-import Sidebar from './Sidebar';
-import Body from './Body';
-import Footer from './Footer'
-import { useStateProvider } from '../utils/StateProvider';
-import axios from 'axios';
-import { reducerCases } from '../utils/Constant';
+import React, { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Body from "./Body";
+import Footer from "./Footer";
+import { useStateProvider } from "../utils/StateProvider";
+import axios from "axios";
+import { reducerCases } from "../utils/Constant";
 
 export default function Spotify() {
-    const [{ token }, dispatch] = useStateProvider();
+  const [{ token }, dispatch] = useStateProvider();
+  const bodyRef = useRef();
+  const [navBackground, setNavBackground] = useState(false);
+  const [headerBackground, setHeaderBackground] = useState(false);
+  const bodyScrolled = () => {
+    bodyRef.current.scrollTop >= 30
+      ? setNavBackground(true)
+      : setNavBackground(false);
+    bodyRef.current.scrollTop >= 268
+      ? setHeaderBackground(true)
+      : setHeaderBackground(false);
+  };
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const { data } = await axios.get("https://api.spotify.com/v1/me", {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      });
+      const userInfo = {
+        userId: data.id,
+        userName: data.display_name,
+      };
 
-
-    useEffect(() => {
-        const getUserInfo = async ()=> {
-            const {data} = await axios.get("https://api.spotify.com/v1/me", {
-                headers: {
-                    Authorization: "Bearer " + token,
-                    "Content-Type": "application/json",
-                },
-            });
-            const userInfo = {
-                userId :data.id,
-                userName :data.display_name,
-        };
-        
-        dispatch({ type: reducerCases.SET_USER, userInfo });
+      dispatch({ type: reducerCases.SET_USER, userInfo });
     };
     getUserInfo();
-    
-},[dispatch,token]);
+  }, [dispatch, token]);
 
-
-   return (
-     <Container>
+  return (
+    <Container>
       <div className="spotify__body">
-           <Sidebar />
-           <div className="body">
-               <Navbar />
-               <div className="body_contents">
-                   <Body />
-              </div>
-            </div>
-         </div>
-       <div className="spotify__footer">
-           <Footer />
+        <Sidebar />
+        <div className="body" ref={bodyRef} onScroll={bodyScrolled}>
+          <Navbar navBackground={navBackground} />
+          <div className="body_contents">
+            <Body headerBackground={headerBackground} />
+          </div>
+        </div>
       </div>
-   </Container>
-   );
+      <div className="spotify__footer">
+        <Footer />
+      </div>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-max-width: 100vw;
-max-height: 100vh;
-overflow: hidden;
-display: grid;
-grid-template-rows: 85vh 15vh;
+  max-width: 100vw;
+  max-height: 100vh;
+  overflow: hidden;
+  display: grid;
+  grid-template-rows: 85vh 15vh;
 
-.spotify__body {
+  .spotify__body {
     display: grid;
     grid-template-columns: 15vw 85vw;
     height: 100%;
     width: 100%;
-    background: linear-gradient(transparent, rgba(0,0,0,1));
+    background: linear-gradient(transparent, rgba(0, 0, 0, 1));
     background-color: rgb(32, 87, 100);
-    .body{
-        height: 100%;
-        width: 100%;
-        overflow: auto;
+    .body {
+      height: 100%;
+      width: 100%;
+      overflow: auto;
     }
-}
-`
-
-
-
+  }
+`;
